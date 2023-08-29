80888284455099eeaccddf9577f83edb37b7b667
Erusagi <141950504+Erusagi@users.noreply.github.com>
2023-08-29 18:00:50 +0300

    Update setup.py

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -6,6 +6,6 @@ setup(name="clean_folder",
       author="Victor Yarcev",
       description="sorting and clearing folder in chosen directory",
       entry_point={
-          "console_scripts": ["clean_folder = clean_folder.clean:main"]
+          "console_scripts": ["clean_folder = clean_folder.clean:sort_files"]
       }
 )
